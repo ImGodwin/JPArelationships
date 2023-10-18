adc64dc67f34e53520451ae22ca7718f705a6caf
Godwin <godwinmail6@gmail.com>
2023-10-18 16:47:23 +0200

    created main, adding functions

--- src/main/java/GodwinE/Application.java
+++ src/main/java/GodwinE/Application.java
@@ -1,15 +1,14 @@
 package GodwinE;
 
-import GodwinE.entities.Event;
-import GodwinE.entities.EventDAO;
-import GodwinE.entities.EventType;
-import GodwinE.entities.JpaUtil;
+import GodwinE.entities.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Set;
 import java.util.UUID;
 
 import static GodwinE.entities.JpaUtil.getEntitiManagerFactory;
@@ -23,21 +22,21 @@ public class Application {
 
         try {
             EventDAO ev = new EventDAO(em);
-            System.out.println("confirm");
+            LocationDAO ld = new LocationDAO(em);
+            ParticipationDAO partDAO = new ParticipationDAO(em);
+            PersonDAO personDAO = new PersonDAO(em);
 
-            Event musicEvent = new Event("Pop Event", LocalDate.now(), "amazing pop event", EventType.PUBLIC,
+            Person person = new Person("Kade", "Will", "e-mail", "birthyear", Gender.M);
+            Location location = new Location("Libertà", "Bari");
+
+            Event newEv = new Event("Dance Event", LocalDate.now(), "Dance Event", EventType.PUBLIC,
                     300);
 
-            ev.save(musicEvent);
 
-            Event bigEvent = ev.getById(UUID.fromString("2f330782-d504-44bf-8ecc-aea92f844fc2"));
-            if (musicEvent != null)
-            {
-                musicEvent.setTitle("Dance Event");
-                System.out.println("before refresh");
-                em.refresh(musicEvent);
-                System.out.println("Refreshed");
-            }
+
+
+
+
 
 
 
